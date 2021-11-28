446c78d553192145fc985e9654f757727da97460
Pablo Perales Molas <pperalesm@gmail.com>
2021-11-28 14:17:22 +0100

    Adding recipes and deleting tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, RecipesComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
